2946be92ec6ef9f73ae7252915e7e8eb6723fec3
FinaNurilAulia <FinaNurilAulia>
2025-06-05 18:31:37 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,27 +1,28 @@
-export default 
-  fetch(request, env) 
-    const url = new URL(request.url);
+export default function handler(req, res) {
+  const url = new URL(req.url, `http://${req.headers.host}`); // URL needs base in Node.js
 
-    if (url.pathname.startsWith("/api/aplikasiabsenfina")) {
-      return Response.json([
-        {
-          id: '1',
-          name: "event 1",
-          place: "pandaan",
-          time: 1741247092,
-        },
-        {
-          id: '2',
-          name: "event 2",
-          place: "sukorejo",
-          time: 1741246092,
-        },
-        {
-          id: '3',
-          name: "event 3",
-          place: "purwosari",
-          time: 1741245092,
-        },
-      ]);
-    }
-  
+  if (url.pathname.startsWith("/api/aplikasiabsenfina")) {
+    return res.status(200).json([
+      {
+        id: '1',
+        name: "event 1",
+        place: "pandaan",
+        time: 1741247092,
+      },
+      {
+        id: '2',
+        name: "event 2",
+        place: "sukorejo",
+        time: 1741246092,
+      },
+      {
+        id: '3',
+        name: "event 3",
+        place: "purwosari",
+        time: 1741245092,
+      },
+    ]);
+  }
+
+  res.status(404).json({ error: "Not found" });
+}
